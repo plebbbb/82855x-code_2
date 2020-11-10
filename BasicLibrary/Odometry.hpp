689df0f590747b6dffae4a33dc21f2f12643dc6a
plebbbb <genericpersonyoudontknow@gmail.com>
2020-11-09 23:08:14 -0500

    V1.01 - Fixed issues, OdometryWheels can act like an array now

--- BasicLibrary/Odometry.hpp
+++ BasicLibrary/Odometry.hpp
@@ -1,7 +1,5 @@
-//A header guard to prevent duplicate declarations. Prevents everything between this and a #endif line from being duped
-#ifndef Odometry_HPP
-#define Odometry_HPP
-
+//A header guard
+#pragma once
 #include "Library.hpp"
 //Library.hpp imports all of Odometry.hpp's code, and here, Odometry.hpp imports library.hpp.
 //You may think that this would lead to the program importing itself in an infinite loop,
@@ -16,26 +14,8 @@ namespace BasicLibrary{
   struct OdometryWheels;
   static double DegreesToRadians(double Degrees);
 
-
   //Ok Cho, I just made the encoder classes. How you want to interface with these is up to you.
-  //unique_ptr.get() gives you the pointer for the array of values
-
-  struct OdometryWheels{
-    //This struct is constructed with bracket notation.
-    //For example, DeadWheel a = {LEFT, RIGHT, REAR};
-    DeadWheel LEFT, RIGHT, REAR;
-
-    //This approach only measures the encoder values once per cycle, ensuring synchronization of measurements
-    std::unique_ptr<std::array<double, 3>> get_distances(){
-      return std::unique_ptr<std::array<double, 3>>(
-        new std::array<double, 3> {
-          LEFT.get_distance_AUTORESET(),
-          RIGHT.get_distance_AUTORESET(),
-          REAR.get_distance_AUTORESET()
-        }
-      );
-    }
-  };
+  //unique_ptr.get() gives you the pointer for the array of values. Put everything below odometrywheels to be safe/
 
   //This is a wrapper class that serves as another class with additional features
   //Here, we provide the feature to automatically convert the angle value of a shaft
@@ -60,20 +40,20 @@ namespace BasicLibrary{
     //Constructor if the encoder is plugged into an ADI Expander
     //The explicit keyword allows for the compiler to automatically convert data specified in incorrect data types
     //Explicit uses functions that take one data type, and return another data type to reach the final value
-    explicit DeadWheel(pros::ext_adi_port_tuple_t portA, pros::ext_adi_port_tuple_t portB, bool direction, float Diameter):
-    Encoder(portA,portB,direction)
+    explicit DeadWheel(pros::ext_adi_port_tuple_t portA, bool direction, float Diameter):
+    Encoder(portA,direction)
     {
       WheelRadius = Diameter/2;
     }
 
     //Returns us the raw angle traversed by the encoder in radians
     double get_radian(){
-      return DegreesToRadians(get_angle();
+      return DegreesToRadians(Encoder.get_value());
     }
 
     //Returns us the effective distance traveled by the attached wheel
     double get_distance(){
-      return WheelRadius*get_angle();
+      return WheelRadius*get_radian();
     }
 
     //get_distance. Note that this resets the encoder as well. Do not call this multiple times per cycle
@@ -89,11 +69,46 @@ namespace BasicLibrary{
     }
   };
 
+  //This is a struct meant to hold the DeadWheels in an convienent to move container
+  //Treat it like an array that has built in functions. It can literally do the [] and the {}
+  struct OdometryWheels{
+    //This struct is constructed with bracket notation.
+    //For example, DeadWheel a = {LEFT, RIGHT, REAR};
+    DeadWheel LEFT, RIGHT, BACK;
+
+    //This approach only measures the encoder values once per cycle, ensuring synchronization of measurements
+    std::unique_ptr<std::array<double, 3>> get_distances(){
+      return std::unique_ptr<std::array<double, 3>>(
+        new std::array<double, 3> {
+          LEFT.get_distance_AUTORESET(),
+          RIGHT.get_distance_AUTORESET(),
+          BACK.get_distance_AUTORESET()
+        }
+      );
+    }
+
+    //This function basically takes control of the [] thing you see in arrays
+    //This turns the object into a psudo-array thats basically an array
+    //Usable values are index 0, 1 2. or 'L', 'R', 'B', or those but lowercase
+    //FYI, this doesn't reset, so you will have to manual reset with the reset function
+    double operator[] (int index){
+        switch(index){
+            case 'L':
+            case 'l':
+            case 0: return LEFT.get_distance();
+            case 'R':
+            case 'r':
+            case 1: return RIGHT.get_distance();
+            case 'B':
+            case 'b':
+            case 2: return LEFT.get_distance();
+        };
+    }
+
+  };
+
   //Converts degrees into radians. Radius*radians = distance traveled
   static double DegreesToRadians(double Degrees){
     return Degrees*(180/M_PI);
   };
 }
-
-
-#endif //Everything from here to Odometry_HPP is guarded
